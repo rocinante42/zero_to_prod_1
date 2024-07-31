1514b3aeac1e5f1edd9891fe660e2b8e125229b6
Roberto Amaya <rovertoo@gmail.com>
2024-07-31 11:26:30 +0200

    refactoring to expose port used

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use std::net::TcpListener;
 
 use zero2prod::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let address = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind port and address");
+    run(address)?.await
 }
